1768869dfcac6d923406936aa2e781dbc7e4b7da
vitormdias <vitormd97@gmail.com>
2016-12-29 23:21:11 -0200

    route config added

---
+++ Angular1/src/client/app/app.module.js
@@ -0,0 +1,25 @@
+(() => {
+    'use strict'
+
+    angular
+        .module('app' , ['ngAnimate' , 'ngRoute'])
+        .config(routeConfig)
+
+    routeConfig.$inject = ['$routeProvider'];
+
+    function routeConfig($routeProvider) {
+        $routeProvider
+            .when('/',{
+                templateUrl: 'app/people/people.html',
+                title: 'people'
+            })
+            .when('/avengers',{
+                templateUrl: 'app/avengers/avengers.html',
+                title: 'avengers'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }
+
+})();
