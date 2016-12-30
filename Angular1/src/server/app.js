b021b9bb3cabfc44c12ac0b63953c603b6e4bb35
vitormdias <vitormd97@gmail.com>
2016-12-29 22:58:58 -0200

    Server side added

---
+++ Angular1/src/server/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const express      = require('express');
+const app          = express();
+const bodyParser   = require('body-parser');
+const routes       = require('./routes')(app);
+const port         = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+
+app.use('/', express.static('./src/client'));
+
+app.listen(port, () => {
+    console.log('Express server listening on port ' + port);
+});
