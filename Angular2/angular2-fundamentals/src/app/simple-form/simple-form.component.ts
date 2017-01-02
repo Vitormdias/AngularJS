3b9495b3128546a1fbe12be13dc66e0bdd752e8a
vitormdias <vitormd97@gmail.com>
2017-01-01 22:49:42 -0200

    simple-form updated

--- Angular2/angular2-fundamentals/src/app/simple-form/simple-form.component.ts
+++ Angular2/angular2-fundamentals/src/app/simple-form/simple-form.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-simple-form',
   template: `
-    <p>
-      simple-form Works!
-    </p>
+    <input #myInput type="text">
+    <button (click)="onClick(myInput.value)" >Click me</button>
   `,
   styles: []
 })
 export class SimpleFormComponent implements OnInit {
+
+  onClick(value) {
+      console.log(value);
+  }
 
   constructor() { }
 
